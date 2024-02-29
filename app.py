283a4051ee7bee4723cdb3b4882e70b89b5213bb
Grifith Sheeba Menon <57436703+grifith11@users.noreply.github.com>
2024-02-29 11:32:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-import fcntl
+from waitress import serve
 
+# Create a Flask application instance
 app = Flask(__name__)
 
+# Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello, World!'
 
+# Run the Flask application with Waitress
 if __name__ == '__main__':
-    app.run()
+    serve(app, host='0.0.0.0', port=5000)
